a084474ce9427a7a1b44103bf12484719f92dea2
corourke <cameron.orourke@gmail.com>
2018-10-28 10:21:10 -0700

    Updated to Meteor 8.0

--- client/main.js
+++ client/main.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Meteor } from 'meteor/meteor'
 
+import { Links } from '../imports/api/links'
 import App from '../imports/ui/App.js'
 
 Meteor.startup(function () {
